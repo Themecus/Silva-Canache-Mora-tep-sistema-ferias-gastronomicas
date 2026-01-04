128c2724434d855f460d2a640875217197852c5b
Themecus <127447399+Themecus@users.noreply.github.com>
2026-01-04 16:37:07 -0400

    Revert "fusion exitosa del micro 1 y 2"

--- src/puestos/dto/create-puesto.dto.ts
+++
@@ -1,7 +0,0 @@
-export class CreatePuestoDto {
-  nombre: string;
-  color: string;
-  emprendedorId: string;  //informacion varia para los puestos
-}
-
-//este archivo DTO o Data Transfer Object es para crear un puesto
