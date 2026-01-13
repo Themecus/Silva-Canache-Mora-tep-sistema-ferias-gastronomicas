cc94fc64da62f74106811fd7cacd7f66f4129a6a
Themecus <127447399+Themecus@users.noreply.github.com>
2026-01-12 20:20:41 -0400

    union exitosa probar para pasarlo a limpio

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,40 +1,42 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PedidosModule } from './pedidos/pedidos.module';
-
-@Module({
-  imports: [
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PuestosModule } from './puestos/puestos.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { PedidosModule } from './pedidos/pedidos.module'; // NUEVO
 import { CustomHttpModule } from './common/http/http.module';
 
 @Module({
   imports: [
-    // Configuración de variables de entorno
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'zxxyc',
-      database: 'feria_gastronomica',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      logging: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USERNAME', 'postgres'),
+        password: configService.get<string>('DB_PASSWORD', ''),
+        database: configService.get<string>(
+          'DB_DATABASE',
+          'feria_gastronomica',
+        ),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE', true),
+        logging: configService.get<boolean>('DB_LOGGING', true),
+      }),
     }),
 
+    CustomHttpModule,
+    PuestosModule,
+    UsuariosModule,
     PedidosModule,
   ],
   controllers: [AppController],
--- src/pedidos/pedidos.module.ts
+++ src/pedidos/pedidos.module.ts
@@ -4,10 +4,15 @@ import { PedidosService } from './pedidos.service';
 import { PedidosController } from './pedidos.controller';
 import { Pedido } from './entities/pedido.entity';
 import { DetallePedido } from './entities/detalle-pedido.entity';
+import { CustomHttpModule } from '../common/http/http.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pedido, DetallePedido])],
+  imports: [
+    TypeOrmModule.forFeature([Pedido, DetallePedido]),
+    CustomHttpModule,
+  ],
   controllers: [PedidosController],
   providers: [PedidosService],
+  exports: [PedidosService],
 })
 export class PedidosModule {}
